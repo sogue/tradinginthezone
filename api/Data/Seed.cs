f869826f6125b7e99124168271e0e78576ec8e77
Soner Güney <soguen@outlook.com>
2020-12-28 19:24:50 +0100

    Fix Typo

--- api/Data/Seed.cs
+++ api/Data/Seed.cs
@@ -15,7 +15,7 @@ namespace API.Data
             if (await context.Users.AnyAsync()) return;
 
             string userData = await System.IO.File.ReadAllTextAsync("Data/UserSeedData.json");
-            List<AppUser>? users = JsonSerializer.Deserialize<List<AppUser>>(userData);
+            List<AppUser> users = JsonSerializer.Deserialize<List<AppUser>>(userData);
             if (users == null) return;
             foreach (AppUser user in users)
             {
